6084089ee5545002eb89123868426d925d136620
BogdanaZ <bogdanamonica@gmail.com>
2021-07-18 02:27:43 +0200

    combine latest

--- src/app/components/merge-users/merge-users.component.ts
+++ src/app/components/merge-users/merge-users.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { merge, combineLatest } from 'rxjs';
+import { delay, startWith } from 'rxjs/operators';
 import { User } from '../../user.model';
 import { API, UserService } from '../../user.service';
 
@@ -15,19 +16,19 @@ export class MergeUsersComponent implements OnInit {
   ngOnInit() {
     merge(
       this._userservice.getUsers(),
-      this._userservice.getUsers(API.usersApiSecond)
+      this._userservice.getUsers(API.usersApiSecond).pipe(delay(5000))
     ).subscribe((users: User[]) => {
       console.log(users);
       this.users = [...this.users, ...users];
     });
 
-    combineLatest(
-      this._userservice.getUsers(),
-      this._userservice.getUsers(API.usersApiSecond)
-    ).subscribe(([usersAPI, usersApiSecond]) => {
-      console.log(usersAPI);
-      console.log(usersApiSecond);
-      this.users = [...usersAPI, ...usersApiSecond];
-    });
+    // combineLatest(
+    //   this._userservice.getUsers(),
+    //   this._userservice.getUsers(API.usersApiSecond).pipe(delay(5000))
+    // ).subscribe(([usersAPI, usersApiSecond]) => {
+    //   console.log(usersAPI);
+    //   console.log(usersApiSecond);
+    //   this.users = [...usersAPI, ...usersApiSecond];
+    // });
   }
 }
